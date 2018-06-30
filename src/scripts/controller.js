dfc067f384d812049128b764abce222ed1aa7dfd
Redvan <33462180+Redvanisation@users.noreply.github.com>
2018-06-30 14:50:12 +0100

    Update controller.js

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,7 +1,7 @@
 // Registering the service worker:
 
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/currencyconverter/sw.js')
+	navigator.serviceWorker.register('/currencyconverter/sw.js', {scope: '/currencyconverter/'})
 	.then(() => console.log('SW Regitered successfully!')) 
 	.catch((err) => console.log('SW Registration Failed!', err))
 }
